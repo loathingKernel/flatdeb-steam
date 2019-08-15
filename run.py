c9919aaf0f46505cc1fe0fa670f7b6cf9b2677cd
Simon McVittie <smcv@collabora.com>
2019-08-15 10:16:29 +0100

    Format debug_symbols as a boolean value when passing it to debos
    Signed-off-by: Simon McVittie <smcv@collabora.com>

--- run.py
+++ run.py
@@ -765,12 +765,10 @@ class Builder:
                 argv.append('-t')
                 argv.append('build_id:{}'.format(self.build_id))
 
-            if self.debug_symbols:
-                argv.append('-t')
-                argv.append('debug_symbols:true')
-            else:
-                argv.append('-t')
-                argv.append('debug_symbols:')
+            argv.append('-t')
+            argv.append(
+                'debug_symbols:{}'.format(str(self.debug_symbols).lower())
+            )
 
             if self.variant_name is not None:
                 argv.append('-t')
